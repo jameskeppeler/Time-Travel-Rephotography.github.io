fa956397ec7a47905640222fff8289daaff9f571
jgke225 <jgke225@uky.edu>
2026-02-26 12:42:08 -0500

    Require SourceRepo parameter in bootstrap script

--- bootstrap_local_assets.ps1
+++ bootstrap_local_assets.ps1
@@ -1,20 +1,18 @@
 param(
-    [string]$SourceRepo = "C:\Users\james\Projects\Time-Travel-Rephotography.github.io"
+    [Parameter(Mandatory = $true)]
+    [string]$SourceRepo
 )
 
 $ErrorActionPreference = "Stop"
 
 Write-Host "Using source repo: $SourceRepo"
 
-# 1) Dataset image used for the verified test
 New-Item -ItemType Directory -Force ".\dataset" | Out-Null
 Copy-Item "$SourceRepo\dataset\Abraham Lincoln_01.png" ".\dataset\" -Force
 
-# 2) Main checkpoint tree
 New-Item -ItemType Directory -Force ".\checkpoint" | Out-Null
 Copy-Item "$SourceRepo\checkpoint\*" ".\checkpoint\" -Recurse -Force
 
-# 3) Face parsing checkpoint required by third_party/face_parsing/test.py
 New-Item -ItemType Directory -Force ".\third_party\face_parsing\res\cp" | Out-Null
 Copy-Item "$SourceRepo\third_party\face_parsing\res\cp\79999_iter.pth" ".\third_party\face_parsing\res\cp\" -Force
 
