72663a6a259c93276d4506caa88d08881d12149d
jgke225 <jgke225@uky.edu>
2026-03-05 09:47:14 -0500

    Add PowerShell launcher for PySide6 GUI

---
+++ launch_gui.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = "Stop"
+
+$repoRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+$guiApp = Join-Path $repoRoot "gui\app.py"
+
+if (-not (Test-Path $guiApp)) {
+    throw "GUI app not found: $guiApp"
+}
+
+conda run -n rephoto_gui python $guiApp
